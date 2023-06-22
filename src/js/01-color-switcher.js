929a67ddc19f44e72a87edf098cc4707db7f859d
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2023-06-22 12:15:54 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(stopBtn)
+const bodyBg = document.querySelector('body')
+
+startBtn.addEventListener('click', onstartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+let timerChange = null;
+ 
+function onstartBtnClick(){
+
+startBtn.disabled = true;
+stopBtn.disabled = false;
+timerChange = setInterval(() => { bodyBg.style.backgroundColor = getRandomHexColor()}, 1000);
+
+}
+
+function onStopBtnClick(){
+    startBtn.disabled = false;
+stopBtn.disabled = true;
+clearInterval(timerChange)
+}
